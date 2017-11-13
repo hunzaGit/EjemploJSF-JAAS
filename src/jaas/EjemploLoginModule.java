55b9a5959f5d5269cfac5889b9fa1eae50a6376b
Rodrigo de Miguel <rodrigo.trazas@gmail.com>
2017-11-13 16:36:26 +0100

    Commit con el proyecto

---
+++ src/jaas/EjemploLoginModule.java
@@ -0,0 +1,122 @@
+package jaas;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.PasswordCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.LoginException;
+import javax.security.auth.spi.LoginModule;
+
+public class EjemploLoginModule implements LoginModule {
+
+	private CallbackHandler handler;
+	private Subject subject;
+	private UserPrincipal userPrincipal;
+	private RolePrincipal rolePrincipal;
+	private String login;
+	private List<String> userGroups;
+
+	@Override
+	public void initialize(Subject subject, CallbackHandler callbackHandler,
+			Map<String, ?> sharedState, Map<String, ?> options) {
+
+		handler = callbackHandler;
+		this.subject = subject;
+	}
+
+	@Override
+	public boolean login() throws LoginException {
+
+		Callback[] callbacks = new Callback[2];
+		callbacks[0] = new NameCallback("login");
+		callbacks[1] = new PasswordCallback("password", true);
+
+		try {
+			handler.handle(callbacks);
+			String name = ((NameCallback) callbacks[0]).getName();
+			String password = String.valueOf(((PasswordCallback) callbacks[1]).getPassword());
+
+			System.out.println("EjemploLoginModule.login");
+
+
+			// Here we validate the credentials against some
+			// authentication/authorization provider.
+			// It can be a Database, an external LDAP, a Web Service, etc.
+			// For this tutorial we are just checking if user is "user123" and
+			// password is "pass123"
+			if (name != null && name.equals("admin") && password != null && password.equals("123")) {
+				login = name;
+				userGroups = new ArrayList<String>();
+				userGroups.add("admin");
+				System.out.println("name = " + name);
+
+				return true;
+			}
+
+			if (name != null && name.equals("usuario") && password != null && password.equals("123")) {
+				login = name;
+				userGroups = new ArrayList<String>();
+				userGroups.add("user");
+				System.out.println("name = " + name);
+				return true;
+			}
+
+
+			if (name != null && name.equals("superuser") && password != null && password.equals("123")) {
+				login = name;
+				userGroups = new ArrayList<String>();
+				userGroups.add("user");
+				userGroups.add("admin");
+				System.out.println("name = " + name);
+				return true;
+			}
+
+			// If credentials are NOT OK we throw a LoginException
+			throw new LoginException("Authentication failed");
+
+		} catch (IOException e) {
+			throw new LoginException(e.getMessage());
+		} catch (UnsupportedCallbackException e) {
+			throw new LoginException(e.getMessage());
+		}
+
+	}
+
+	@Override
+	public boolean commit() throws LoginException {
+		System.out.println("EjemploLoginModule.commit");
+
+		userPrincipal = new UserPrincipal(login);
+		subject.getPrincipals().add(userPrincipal);
+
+		if (userGroups != null && userGroups.size() > 0) {
+			System.out.println("userGroups = " + userGroups);
+			for (String groupName : userGroups) {
+				rolePrincipal = new RolePrincipal(groupName);
+				subject.getPrincipals().add(rolePrincipal);
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean abort() throws LoginException {
+		return false;
+	}
+
+	@Override
+	public boolean logout() throws LoginException {
+		subject.getPrincipals().remove(userPrincipal);
+		subject.getPrincipals().remove(rolePrincipal);
+		return true;
+	}
+
+}
